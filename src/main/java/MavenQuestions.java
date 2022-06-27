4133ad3ee06fd0b4aaddf3a6461548f2607b4743
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-27 11:32:48 -0500

    add completed MavenQuestions

---
+++ src/main/java/MavenQuestions.java
@@ -0,0 +1,24 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MavenQuestions {
+    public static void main(String[] args) {
+        int input =0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Sometheing: ");
+        if (sc.hasNextInt()){
+            input = sc.nextInt();
+            System.out.println("You entered a number!\nYour number is:Yo " + input );
+        } else {
+
+            String input1 = sc.nextLine();
+            System.out.println("You entered: " + input1);
+            System.out.println("Flipped Case: " + StringUtils.swapCase(input1));
+            System.out.println("Reversed: " + StringUtils.reverse(input1));
+
+        }
+
+    }
+}
